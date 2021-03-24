3abe6d8056886a3403b0e6190c50c53e6368acfe
Matt <b6020254@my.shu.ac.uk>
2021-03-24 14:20:56 +0000

    Auto stash before checking out "origin/develop"
    Created New GameManager

---
+++ Source/MyProject/Public/WarhouseGameManager.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BillboardComponent.h"
+#include "WarhouseGameManager.generated.h"
+
+UCLASS()
+class MYPROJECT_API AWarhouseGameManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWarhouseGameManager();
+
+	UPROPERTY(EditAnywhere)
+		class APackageManager* PackageManager; //reference to the package manager class
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	//A UBillboardComponent to hold Icon sprite
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Billboard")
+		UBillboardComponent* BillboardComponent;
+	//Sprite for the Billboard Component
+	UTexture2D* SpriteTexture;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere)
+		float GameTimer;
+	UPROPERTY(VisibleAnywhere)
+		float PackageTimer;
+
+	void SpawnPackage();
+
+};
