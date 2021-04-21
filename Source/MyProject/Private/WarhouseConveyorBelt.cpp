1f21d270caefcfd0e42d82c874b1d4812de72727
Matt <b6020254@my.shu.ac.uk>
2021-04-21 13:45:32 +0100

    Added: Mesh and Box components

--- Source/MyProject/Private/WarhouseConveyorBelt.cpp
+++ Source/MyProject/Private/WarhouseConveyorBelt.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "WarhouseConveyorBelt.h"
@@ -6,16 +6,26 @@
 // Sets default values
 AWarhouseConveyorBelt::AWarhouseConveyorBelt()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	//mesh
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
+	RootComponent = BaseMesh;
+	//box component
+	boxComponent = CreateDefaultSubobject<UBoxComponent>(FName("Collision Mesh"));
+	boxComponent->SetWorldLocation(GetActorLocation());
+	boxComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	boxComponent->OnComponentBeginOverlap.AddDynamic(this, &AWarhouseConveyorBelt::OnOverlapBegin);
+	boxComponent->OnComponentEndOverlap.AddDynamic(this, &AWarhouseConveyorBelt::OnOverlapEnd);
+	boxComponent->SetBoxExtent(FVector(158, 128, 60));
+	boxComponent->SetRelativeLocation(FVector(380, 0, 70));
 }
 
 // Called when the game starts or when spawned
 void AWarhouseConveyorBelt::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -25,3 +35,14 @@ void AWarhouseConveyorBelt::Tick(float DeltaTime)
 
 }
 
+void AWarhouseConveyorBelt::MoveObjectOnBelt()
+{
+}
+
+void AWarhouseConveyorBelt::OnOverlapBegin(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void AWarhouseConveyorBelt::OnOverlapEnd(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
--- Source/MyProject/Public/WarhouseConveyorBelt.h
+++ Source/MyProject/Public/WarhouseConveyorBelt.h
@@ -1,26 +1,38 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
 #include "WarhouseConveyorBelt.generated.h"
 
 UCLASS()
 class MYPROJECT_API AWarhouseConveyorBelt : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AWarhouseConveyorBelt();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+		UStaticMeshComponent* BaseMesh;
+	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
+		UBoxComponent* boxComponent;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	UFUNCTION()
+		void MoveObjectOnBelt();
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlapComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+		void OnOverlapEnd(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 };
