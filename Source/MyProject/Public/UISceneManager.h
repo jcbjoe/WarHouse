e016b94de305334f04184a600e1db93af054f5b3
JoeGarlick <joegarlick28@hotmail.com>
2021-05-05 23:50:33 +0100

    Fixed: UIScene manager

--- Source/MyProject/Public/UISceneManager.h
+++ Source/MyProject/Public/UISceneManager.h
@@ -16,7 +16,6 @@ public:
 	// Sets default values for this actor's properties
 	AUISceneManager();
 	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 	void UnloadCurrentWidget();
 
 	UFUNCTION(BlueprintCallable)
@@ -24,7 +23,7 @@ public:
 protected:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
